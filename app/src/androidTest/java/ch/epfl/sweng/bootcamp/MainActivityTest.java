b9f1d4abcb57b5ed2fdc8643ee28877ab69270d0
Samuel Chassot <samuel.chassot@epfl.ch>
2018-09-22 13:34:39 +0200

    add a return to force Travis to reprocess

--- app/src/androidTest/java/ch/epfl/sweng/bootcamp/MainActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/bootcamp/MainActivityTest.java
@@ -16,6 +16,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
+    
     @Rule
     public final ActivityTestRule<MainActivity> mActivityRule =
             new ActivityTestRule<>(MainActivity.class);
